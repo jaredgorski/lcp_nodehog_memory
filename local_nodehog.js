4b74a4ddc0fbcc395e38ae55d8c736e55d49ecd7
jaredgorski <jared.gorski@liferay.cloud>
2019-10-07 17:28:59 -0700

    Test

--- local_nodehog.js
+++ local_nodehog.js
@@ -5,6 +5,33 @@ class NodeHog {
     this.deathspan = parseInt(deathspan, 10) || 600000;
     this.iterations = parseInt(iterations, 10) || 10;
     this.pid = Math.random().toString(36).substr(2, 5);
+
+    this.acc = this.type === 'memory' ? [] : 0;
+    this.periodType = this.lifespan > 60000 ? 'minute' : 'second';
+    this.period = this.periodType === 'minute' ? 60000 : 1000;
+    this.periodCount = 0;
+    this.startTime = 0;
+    this.loggerInc = 0;
+    this.toggle = false;
+
+    this.reset = () => {
+      this.periodCount = 0;
+      this.startTime = 0;
+      this.loggerInc = 0;
+      this.toggle = false;
+
+      if (this.type === 'memory') {
+        this.acc = [];
+      } else if (this.type === 'cpu') {
+        this.acc = 0;
+      }
+
+      this.intervals.forEach(interval => clearInterval(interval));
+      this.timeouts.forEach(timeout => clearTimeout(timeout));
+    };
+
+    this.intervals = [];
+    this.timeouts = [];
   }
   
   async start() {
@@ -21,90 +48,93 @@ class NodeHog {
   }
 
   stress() {
-    const periodType = this.lifespan > 60000 ? 'minute' : 'second';
-    const period = periodType === 'minute' ? 60000 : 1000;
-    let start = Date.now(),
-      now = start,
-      loggerInc = start,
-      periodCount = 0,
-      acc = this.type === 'memory' ? [] : 0;
+    this.startTime = Date.now(),
+    this.loggerInc = this.startTime;
 
-    const resource = this.type === 'memory' ? 'MEMORY' : 'CPU';
-    console.log('\n[ ' + this.pid + ' ] --> Stressing ' + resource + '...\n');
-
-    const logger = (force) => {
-      const timeDiff = now - loggerInc;
-
-      if (timeDiff > period || force) {
-        periodCount++;
-        const plural = periodCount > 1 ? 's' : '';
-
-        console.log('[ ' + this.pid + ' ] ----> ' + 
-          periodCount + 
-          ' ' + 
-          periodType + 
-          plural + 
-          ' of stress period complete.');
-
-        loggerInc = Date.now();
-      }
-    };
-
-    const stressFn = () => {
-      if (this.type === 'cpu') {
-        while (now - start < this.lifespan) {
-          acc += Math.random() * Math.random();
-
-          logger();
-          now = Date.now();
-        }
-      } else if (this.type === 'memory') {
-        const v8 = require('v8');
-        v8.setFlagsFromString('--max_old_space_size=16384');
-
-        const stressMem = () => {
-          const {heapTotal, heapUsed} = process.memoryUsage()
-          const available8Bits = (((heapTotal - heapUsed) / 8) - 100000).toFixed(0);
-
-          if (available8Bits > 0) {
-            const heapHog = new Float64Array(available8Bits);
-            acc.push(heapHog);
-          }
-        }
-
-        const int = setInterval(() => {
-          const timeDiff = now - loggerInc;
-
-          if (timeDiff > this.lifespan) {
-            clearInterval(int);
-          }
-
-          stressMem();
-
-          logger();
-          now = Date.now();
-        }, 100);
-      }
-    };
+    console.log('\n[ ' + this.pid + ' ] --> Stressing ' + this.type.toUpperCase() + '...\n');
 
     return new Promise(resolve => {
-      setTimeout(() => {
-        logger(true);
-        resolve();
-      }, this.lifespan);
+      const endStressTime = this.lifespan / 1000;
+      let endStressInc = 0;
+      let toggle = true;
+      const maybeEnd = callback => {
+        if (endStressInc > endStressTime) {
+          logger(true);
+          this.reset();
+          resolve();
+        }
+      };
 
-      stressFn();
+      this.intervals.push(setInterval(() => {
+        maybeEnd();
+        this.logger();
+        endStressInc++;
+      }, 1000));
+
+      if (this.type === 'cpu') {
+        this.stressCpu();
+      } else if (this.type === 'memory') {
+        this.stressMemory();
+      }
     });
-    
+  }
+
+  stressCpu() {
+    this.toggle = true;
+    while (this.toggle) {
+      this.acc += Math.random() * Math.random();
+    }
+  }
+
+  stressMemory() {
+    const v8 = require('v8');
+    v8.setFlagsFromString('--max_old_space_size=32768');
+
+    const stressMem = () => {
+      const {heapTotal, heapUsed} = process.memoryUsage()
+      const cushion = 10000;
+      const available8Bits = (((heapTotal - heapUsed) / 8) - cushion).toFixed(0);
+
+      if (available8Bits > 0) {
+        const heapHog = new Float64Array(available8Bits);
+        this.acc.push(heapHog);
+      }
+    }
+
+    this.intervals.push(setInterval(() => {
+      stressMem();
+    }, 50));
   }
 
   relieve() {
     console.log('\n[ ' + this.pid + ' ] --> Relieving...\n');
 
     return new Promise(resolve => {
-      setTimeout(resolve, this.deathspan);
+      this.timeouts.push(setTimeout(() => {
+        this.reset();
+        resolve();
+      }, this.deathspan));
     });
   }
+
+  logger(force) {
+    const now = Date.now();
+    const timeDiff = now - this.loggerInc;
+
+    if (timeDiff > this.period || force) {
+      this.periodCount++;
+      const plural = this.periodCount > 1 ? 's' : '';
+
+      console.log('[ ' + this.pid + ' ] ----> ' + 
+        this.periodCount + 
+        ' ' + 
+        this.periodType + 
+        plural + 
+        ' of stress period complete.');
+
+      this.loggerInc = Date.now();
+    }
+  };
 }
 
 module.exports = NodeHog;
